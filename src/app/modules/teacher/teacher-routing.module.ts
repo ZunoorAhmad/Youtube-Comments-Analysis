122b9b6756c4386efdca7844385b6e790325f204
Zunoor Ahmad <zunoor19@gmail.com>
2024-12-31 19:02:32 +0500

    uodate dashboard

--- src/app/modules/teacher/teacher-routing.module.ts
+++ src/app/modules/teacher/teacher-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
         pathMatch: 'full',
     },
     {
-        path: 'profile',
+        path: '',
         component: TeacherComponent,
         children: [
             {
